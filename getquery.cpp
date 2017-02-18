909e738353ab983e3b26f78fb179bb6eb88d22b6
Jim Read <jimmydean886@hotmail.com>
2017-02-18 11:24:38 -0500

    Put everything in a namespace, library checking
    Added a namespace cgim to prevent this project from stepping on the toes
    of potentially larger projects it may be useful for, and to allow for a
    greater range of names inside the program should we accidentally stumble
    on something used in a standard manner
    
    Added preprocessor directives which check for iostream and cstdlib
    inclusion, and don't include them if they're already there.

--- getquery.cpp
+++ getquery.cpp
@@ -1,17 +1,25 @@
-#include<cstdlib>
+#ifndef _GLIBCXX_IOSTREAM
+#include<iostream>
+#endif
 
-string cgimanager_getqstring(){
-	string querytype = getenv("REQUEST_METHOD");
-	string query;
-	if(querytype == "GET"){
-		query = getenv("QUERY_STRING");
-	} else {
-		string qsection;
-		cin>>query;
-		while(!cin.fail()){
-			cin>>qsection;
-			query+=" "+qsection;
+#ifndef _GLIBCXX_CSTDLIB
+#include<cstdlib>
+#endif
+
+namespace cgim{
+	std::string getqstring(){
+		std::string querytype = getenv("REQUEST_METHOD");
+		std::string query;
+		if(querytype == "GET"){
+			query = getenv("QUERY_STRING");
+		} else {
+			std::string qsection;
+			std::cin>>query;
+			while(!std::cin.fail()){
+				std::cin>>qsection;
+				query+=" "+qsection;
+			}
 		}
+		return query;
 	}
-	return query;
 }
